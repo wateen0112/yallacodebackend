45b83838ecc5f8c9a9c042e025581fe8d2482adb
wateen0112 <wateenoaswad2000@gmail.com>
2026-05-03 00:31:54 +0300

    port chaned

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const swaggerDefinition = {
     },
     servers: [
         {
-            url: `http://localhost:${process.env.PORT || 5000}`,
+            url: `http://localhost:${process.env.PORT || 3000}`,
             description: 'Development server',
         },
     ],
